c4d5986e7a850b8c4cc868c2e0c6cb6f0c1fa613
Clément Chaine <clement.chaine25@gmail.com>
2024-03-10 14:56:29 +0100

    Adds html generation to the build system (#62)

--- cmake/ecap5-treq.cmake
+++ cmake/ecap5-treq.cmake
@@ -11,11 +11,17 @@ endif()
 
 # Define commands for using ECAP5-TREQ
 add_custom_command(
+  OUTPUT report.html
+  DEPENDS ${TEST_OUTPUTS}
+  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+  COMMAND ${ecap5_treq_EXECUTABLE} -c ${CMAKE_SOURCE_DIR}/config/treq.json gen_report -o ${CMAKE_BINARY_DIR}/report.html --html)
+add_custom_target(report DEPENDS report.html)
+add_custom_command(
   OUTPUT report.md
   DEPENDS ${TEST_OUTPUTS}
   WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
   COMMAND ${ecap5_treq_EXECUTABLE} -c ${CMAKE_SOURCE_DIR}/config/treq.json gen_report -o ${CMAKE_BINARY_DIR}/report.md)
-add_custom_target(report DEPENDS report.md)
+add_custom_target(report_markdown DEPENDS report.md)
 add_custom_command(
   OUTPUT test-result-badge.json
   DEPENDS ${TEST_OUTPUTS}
