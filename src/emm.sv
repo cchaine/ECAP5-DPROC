a125dc68d1d6af70bcc6562642b9c246cd75eaa1
Clément Chaine <clement.chaine25@gmail.com>
2024-04-14 21:07:02 +0000

    Reimplement the emm using a state machine

--- src/emm.sv
+++ src/emm.sv
@@ -65,14 +65,18 @@ module emm import ecap5_dproc_pkg::*;
   input   logic        m_wb_stall_i
 );
 
+typedef enum logic [2:0] {
+  IDLE,      // 0
+  SWITCHING, // 1
+  REQUEST   // 2
+} state_t; 
+state_t state_d, state_q;
+
 logic switch_d, switch_q;
-logic in_request_d, in_request_q;
 logic s1_request, s2_request;
 
-assign s1_request = s1_wb_stb_i && s1_wb_cyc_i;
-assign s2_request = s2_wb_stb_i && s2_wb_cyc_i;
-
-logic s2_stall_q;
+logic s1_stall_d, s1_stall_q, 
+      s2_stall_d, s2_stall_q;
 
 logic[31:0] sel_wb_adr;
 logic[31:0] sel_wb_dat_o;
@@ -82,54 +86,102 @@ logic       sel_wb_stb;
 logic       sel_wb_ack;
 logic       sel_wb_cyc;
 
+
+assign s1_request = s1_wb_stb_i && s1_wb_cyc_i;
+assign s2_request = s2_wb_stb_i && s2_wb_cyc_i;
+
+always_comb begin : state_machine
+  state_d = state_q;
+
+  switch_d = switch_q;
+
+  s1_stall_d = s1_stall_q;
+  s2_stall_d = s2_stall_q;
+
+  case(state_q)
+    IDLE: begin 
+      // if correct interface, go to request
+      if(s1_request) begin
+        state_d = REQUEST;
+      end
+      // if switch needed, go to switching
+      if(s2_request) begin
+        switch_d = 1;
+        s1_stall_d = 1;
+        state_d = SWITCHING;
+      end
+    end
+    SWITCHING: begin
+      // Unstall the granted interface
+      if(~switch_q) begin
+        s1_stall_d = 0;
+      end else begin
+        s2_stall_d = 0;
+      end
+      state_d = REQUEST;
+    end
+    REQUEST: begin
+      // Request is done
+      if(sel_wb_cyc == 0) begin
+        // If the other wants access
+        if((~switch_q && s2_request) || (switch_q && s1_request)) begin
+          switch_d = ~switch_q;
+          // Stall the current interface
+          if(~switch_q) begin
+            s1_stall_d = 1;
+          end else begin
+            s2_stall_d = 1;
+          end
+          state_d = SWITCHING; 
+        end else begin
+          // Switch back to default and idle
+          switch_d = 0;
+          s1_stall_d = 0;
+          s2_stall_d = 1;
+          state_d = IDLE;
+        end
+      end
+    end
+    default: begin end
+  endcase
+end
+
 always_comb begin
-  if(~switch_q || (switch_q && s2_stall_q)) begin
+  if(~switch_q && ~s1_stall_q) begin
     sel_wb_adr    =  s1_wb_adr_i;
     sel_wb_dat_o  =  s1_wb_dat_i;
     sel_wb_we     =  s1_wb_we_i;
     sel_wb_sel    =  s1_wb_sel_i;
     sel_wb_stb    =  s1_wb_stb_i;
     sel_wb_cyc    =  s1_wb_cyc_i;
-  end else begin
+  end else if(switch_q && ~s2_stall_q) begin
     sel_wb_adr    =  s2_wb_adr_i;
     sel_wb_dat_o  =  s2_wb_dat_i;
     sel_wb_we     =  s2_wb_we_i;
     sel_wb_sel    =  s2_wb_sel_i;
     sel_wb_stb    =  s2_wb_stb_i;
     sel_wb_cyc    =  s2_wb_cyc_i;
+  end else begin
+    sel_wb_adr    =  '0; 
+    sel_wb_dat_o  =  '0; 
+    sel_wb_we     =   0; 
+    sel_wb_sel    =  '0; 
+    sel_wb_stb    =   0; 
+    sel_wb_cyc    =   0; 
   end
 end
 
-assign in_request_d = sel_wb_stb && sel_wb_cyc && m_wb_stall_i == 0;
-
 always_ff @(posedge clk_i) begin
   if(rst_i) begin
+    state_q <= IDLE;
     switch_q <= 0;
+    s1_stall_q <= 0;
     s2_stall_q <= 1;
-    in_request_q <= 0;
   end else begin
-    // Begining of request
-    if(m_wb_stall_i == 0 && ((~switch_q && s1_request) || (switch_q && s2_request))) begin
-      in_request_q <= 1;
-    end
-    // End of request
-    if(in_request_q && sel_wb_cyc == 0) begin
-      in_request_q <= 0;
-      if(s2_request) begin
-        switch_q <= 1;
-      end else begin
-        switch_q <= 0;
-      end
-      s2_stall_q <= 1;
-    end
-    // s2 bus request
-    if(in_request_q == 0 && s2_request) begin
-      switch_q <= 1;
-    end
-    // s2 bus grant
-    if(switch_q && s2_request) begin
-      s2_stall_q <= 0;
-    end
+    state_q <= state_d;
+    switch_q <= switch_d;
+    s1_stall_q <= s1_stall_d;
+    s2_stall_q <= s2_stall_d;
   end
 end
 
@@ -146,7 +198,7 @@ assign s2_wb_dat_o =  switch_q ? m_wb_dat_i : '0;
 assign s1_wb_ack_o = ~switch_q ? m_wb_ack_i : 0;
 assign s2_wb_ack_o =  switch_q ? m_wb_ack_i : 0;
 
-assign s1_wb_stall_o = switch_q;
+assign s1_wb_stall_o = s1_stall_q;
 assign s2_wb_stall_o = s2_stall_q;
 
 endmodule // exm
